c05a9ea78f5d88f433106168cb977f00dcd9a17f
Art Bluefence <37248161+Hamsterlicious88@users.noreply.github.com>
2023-03-18 21:16:05 -0400

    Tophat contrast
    Contrast enhancement for images

---
+++ tophat_contrast.m
@@ -0,0 +1,16 @@
+clc; clear all; close all;
+imag=double(importdata('image_49.tif'));
+gain=double(importdata('Gain.tif'));
+off=double(importdata('offset.tif'));
+gi=gain-imag;
+ig=imag-gain;
+go=gain-off;
+og=off-gain;
+io=imag-off;
+oi=off-imag;
+oiog=oi./og;
+ioog=io./og;
+gigo=gi./go;
+iggo=ig./go;
+igth=imtophat(iggo,strel('disk',100));
+gith=imtophat(gigo,strel('disk',100));
