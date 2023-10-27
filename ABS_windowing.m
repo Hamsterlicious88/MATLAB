5bfa29f0b744e0bc05aa01c0e14e20256b579399
Hamsterlicious88 <Hamsterlicious88@gmail.com>
2023-10-27 12:40:34 -0400

    minor updates to windowing process

--- ABS_windowing.m
+++ ABS_windowing.m
@@ -20,12 +20,12 @@ CMesh1=(Mesh1-Offset1)./(Gain1-Offset1);
 
 %%
 %%
-xmin=1;
-xmax=2048;
-
+xmin=600;
+xmax=3*xmin;
+cropdim=10;
 for i = xmin:xmax
     for j = xmin:xmax
-        [CObji,recti] = imcrop(CObj1,[i,j,9,9]);
+        [CObji,recti] = imcrop(CObj1,[i,j,cropdim,cropdim]);
         [Meshi]     = imcrop(CMesh1,recti);
         avg=mean2(CObji)/mean2(Meshi);
         ratio=real(ifft2(fft2(CObji)./fft2(Meshi)));
@@ -34,18 +34,20 @@ for i = xmin:xmax
         blankslate2(j,i)=ratio;
     end
 end
-figure; imagesc(imcrop(blankslate1,[800,800,800,800])); colormap gray; axis image
-figure; imagesc(imcrop(blankslate2,[800,800,800,800])); colormap gray; axis image
+figure; imagesc(imcrop(blankslate1,[xmin,xmin,xmax-xmin,xmax-xmin])); colormap gray; axis image
+figure; imagesc(imcrop(blankslate2,[xmin,xmin,xmax-xmin,xmax-xmin])); colormap gray; axis image
 clear CObji
 clear ratio
 clear avg
+clear cropdim
 %%
 %%
 xmin=1;
-xmax=2048;
+xmax=2049;
+cropdim=10;
 for i = xmin:xmax
     for j = xmin:xmax
-        [CObji,recti] = imcrop(Obj1,[i,j,9,9]);
+        [CObji,recti] = imcrop(Obj1,[i,j,cropdim,cropdim]);
         [Meshi]     = imcrop(Mesh1,recti);
         avg=mean2(CObji)/mean2(Meshi);
         ratio=real(ifft2(fft2(CObji)./fft2(Meshi)));
@@ -54,20 +56,22 @@ for i = xmin:xmax
         blankslate4(j,i)=ratio;
     end
 end
-figure; imagesc(imcrop(blankslate3,[800,800,800,800])); colormap gray; axis image
-figure; imagesc(imcrop(blankslate4,[800,800,800,800])); colormap gray; axis image
+figure; imagesc(imcrop(blankslate3,[xmin,xmin,xmax-xmin,xmax-xmin])); colormap gray; axis image
+figure; imagesc(imcrop(blankslate4,[xmin,xmin,xmax-xmin,xmax-xmin])); colormap gray; axis image
 clear CObji
 clear ratio
 clear avg
+clear cropdim
 %%
 %%
-rdiv=Obj1./Mesh1;
-ldiv=Obj1.\Mesh1;
+rdiv=CObj1./CMesh1;
+ldiv=CMesh1.\CObj1;
 xmin=1;
 xmax=2048;
+cropdim=10;
 for i = xmin:xmax
     for j = xmin:xmax
-        [CObji,recti] = imcrop(rdiv,[i,j,9,9]);
+        [CObji,recti] = imcrop(rdiv,[i,j,cropdim,cropdim]);
         [Meshi]     = imcrop(ldiv,recti);
         avg=mean2(CObji)/mean2(Meshi);
         ratio=real(ifft2(fft2(CObji).*fft2(Meshi)));
@@ -76,8 +80,8 @@ for i = xmin:xmax
         blankslate6(j,i)=ratio;
     end
 end
-figure; imagesc(imcrop(blankslate5,[800,800,800,800])); colormap gray; axis image
-figure; imagesc(imcrop(blankslate6,[800,800,800,800])); colormap gray; axis image
+figure; imagesc(imcrop(blankslate5,[xmin,xmin,xmax-xmin,xmax-xmin])); colormap gray; axis image
+figure; imagesc(imcrop(blankslate6,[xmin,xmin,xmax-xmin,xmax-xmin])); colormap gray; axis image
 clear CObji
 clear ratio
 clear avg
